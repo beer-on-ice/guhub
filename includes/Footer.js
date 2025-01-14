2f922afcf921287d625d2d3cf92a51ce0d8fa82c
啤酒加冰 <aifound@88.com>
2025-01-14 16:37:08 +0800

    Update Footer.js

--- includes/Footer.js
+++ includes/Footer.js
@@ -10,9 +10,9 @@ export default function Footer() {
         <a data-tippy-content="Vercel" href="https://vercel.com/" title="Vercel" target="_blank" className="hover:text-black transition duration-3000"><TbBrandVercel /></a>&nbsp;
         <a data-tippy-content="Tailwind.css" href="https://tailwindcss.com/" title="Tailwind" target="_blank" className="hover:text-black transition duration-3000"><TbBrandTailwind /></a></p>
 
-      <p style={{display: 'flex'}}><a target="_blank" href="https://notbyai.fyi" rel="noopener"><img id="notbyai" alt="Written by Human, Not by AI" style={{height: '30px',paddingLeft: '10px'}} src="https://notbyai.fyi/img/written-by-human-not-by-ai-white.svg" /></a><a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="noopener"><img id="notbyai" alt="Written by Human, Not by AI" style={{height: '30px',paddingLeft: '10px'}} src="https://imgs.sob.im/file/8e3354cb440875186be1e.jpg" /></a></p>
+      <p style={{display: 'flex'}}><a target="_blank" href="https://notbyai.fyi" rel="noopener"><img id="notbyai" alt="Written by Human, Not by AI" style={{height: '30px',paddingLeft: '10px'}} src="https://notbyai.fyi/img/written-by-human-not-by-ai-white.svg" /></a><a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="noopener"><img id="notbyai" alt="Written by Human, Not by AI" style={{height: '30px',paddingLeft: '10px'}} src="https://imgs.666986.xyz/file/8e3354cb440875186be1e.jpg" /></a></p>
 
-      <p><a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a> &copy; 2019-{moment().format('YYYY')} Lip <a href="https://icp.gov.moe/?keyword=20238181">萌ICP备20238181号</a></p>
+      <p><a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a> &copy; 2019-{moment().format('YYYY')} Lip <a href="https://icp.gov.moe/?keyword=20258883">萌ICP备20258883号</a></p>
     </footer>
   )
 }
